45295a154565b564d6c727699a411295e6a21e27
guilhermeclementedev <guilhermeclementedealmeida@gmail.com>
2025-07-20 11:55:09 -0300

    style: :lipstick: ft_error norminette

--- philo/ft_error.c
+++ philo/ft_error.c
@@ -6,11 +6,12 @@
 /*   By: guclemen <guclemen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 16:23:35 by guclemen          #+#    #+#             */
-/*   Updated: 2025/07/17 17:49:24 by guclemen         ###   ########.fr       */
+/*   Updated: 2025/07/20 11:53:43 by guclemen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
 int	ft_simple_error(char *msg, int ret)
 {
 	if (msg)
@@ -20,41 +21,45 @@ int	ft_simple_error(char *msg, int ret)
 	}
 	return (ret);
 }
-int	ft_error(char *msg, int ret, t_data *data)
+
+static void	free_forks(t_data *data)
 {
 	int	i;
 
 	i = 0;
-	if (msg)
+	while (i < data->num_philos)
 	{
-		ft_putstr_fd("Error :(\n", 2);
-		ft_putstr_fd(msg, 2);
+		pthread_mutex_destroy(&data->forks[i]);
+		i++;
 	}
+	free(data->forks);
+}
+
+static void	free_philos(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->num_philos)
+	{
+		pthread_mutex_destroy(&data->philos[i].mutex_im_dead);
+		pthread_mutex_destroy(&data->philos[i].mutex_meals);
+		i++;
+	}
+	free(data->philos);
+}
+
+int	ft_error(char *msg, int ret, t_data *data)
+{
+	ft_simple_error(msg, ret);
 	if (data)
 	{
 		if (data->forks)
-		{
-			while (i < data->num_philos)
-			{
-				pthread_mutex_destroy(&data->forks[i]);
-				i++;
-			}
-			free(data->forks);
-		}
+			free_forks(data);
 		if (data->philos)
-		{
-			i = 0;
-			while (i < data->num_philos)
-			{
-				pthread_mutex_destroy(&data->philos[i].mutex_im_dead);
-				pthread_mutex_destroy(&data->philos[i].mutex_meals);
-				i++;
-			}
-			free(data->philos);
-		}
+			free_philos(data);
 		pthread_mutex_destroy(&data->start_mutex);
 		pthread_mutex_destroy(&data->print_mutex);
 	}
 	return (ret);
 }
-
